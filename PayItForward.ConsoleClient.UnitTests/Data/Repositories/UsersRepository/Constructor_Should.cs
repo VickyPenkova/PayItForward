3f8d9023a25fcc1d2377b65ac4e26f1d23de7435
Viktoria Penkova <Viktoria.Penkova@fourth.com>
2018-06-05 10:52:52 +0300

    XUnit test for UsersRepository started.

---
+++ PayItForward.ConsoleClient.UnitTests/Data/Repositories/UsersRepository/Constructor_Should.cs
@@ -0,0 +1,24 @@
+﻿namespace PayItForward.UnitTests.Data.Repositories.UsersRepository
+{
+    using System;
+    using PayItForward.Data;
+    using Xunit;
+    using Dbmodels = PayItForward.Data.Models;
+
+    public class Constructor_Should
+    {
+        private readonly PayItForwardDbContext context;
+
+        public Constructor_Should()
+        {
+            this.context = new PayItForwardDbContext();
+        }
+
+        [Fact]
+        public void NotAcceptEmptyNameInConstructor()
+        {
+            // Arrange, Act, Assert
+            Assert.Throws<ArgumentException>(() => new UsersRepository<Dbmodels.User, string>(null));
+        }
+    }
+}
---
+++ PayItForward.ConsoleClient.UnitTests/Data/Repositories/UsersRepository/GetByIdAsync_Should.cs
@@ -0,0 +1,21 @@
+﻿namespace PayItForward.UnitTests.Data.Repositories.UsersRepository
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using PayItForward.Data;
+    using Xunit;
+    using Dbmodels = PayItForward.Data.Models;
+
+    public class GetByIdAsync_Should
+    {
+        private readonly UsersRepository<Dbmodels.User, string> usersRepository;
+
+        public GetByIdAsync_Should()
+        {
+            this.usersRepository = new UsersRepository<Dbmodels.User, string>(new PayItForwardDbContext());
+        }
+
+        private DbSet<Dbmodels.User> DbSet { get; set; }
+    }
+}
